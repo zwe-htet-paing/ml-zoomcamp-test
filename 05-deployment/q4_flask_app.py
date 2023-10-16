8afc7eca9951078238234e98f88285c84fe5c32c
zwe-htet-paing <zwehtetpaing.works@gmail.com>
2023-10-16 14:20:09 +0630

    homework-05

---
+++ 05-deployment/q4_flask_app.py
@@ -0,0 +1,30 @@
+import pickle
+
+from flask import Flask, request, jsonify
+
+def load(filename:str):
+    with open(filename, 'rb') as f_in:
+        return pickle.load(f_in)
+    
+dv = load('dv.bin')
+model = load('model1.bin')
+
+app = Flask('Bank credit scoring')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    client = request.get_json()
+    
+    X = dv.transform([client])
+    y_pred = model.predict_proba(X)[0, 1]
+    score = y_pred >= 0.5
+    
+    result = {
+        'probability': float(y_pred),
+        'get_credict': bool(score)
+    }
+    
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9696)
